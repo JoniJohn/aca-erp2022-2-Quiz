aa8e97a935208b036cca9966958d5d7f49dcde9a
Lesley Thuto Johnson <whosekid@gmail.com>
2022-07-20 13:03:20 +0200

    Code Refactor

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -8,27 +8,24 @@ function Home() {
     return (
         <div>
             <header className="App-header">
-            <h1>Welcome to ACA Quiz.</h1>
-            <Space></Space>
-            <div className='border rounded p-3'>
-            <h5>Please click Start to begin the quiz.</h5>
-            <Space></Space>
-            
-
-            <Link to="/question/1"  className="btn btn-info">
-                Start
-            </Link>
-            
-            </div>
-            <Space></Space>
-            <a
-            className="App-link"
-            href="https://africacode.org"
-            target="_blank"
-            rel="noopener noreferrer"
-            >
-            Lesley Loves ACA
-            </a>
+                <h1>Welcome to ACA Quiz.</h1>
+                <Space></Space>
+                <div className='border rounded p-3'>
+                <h5>Please click Start to begin the quiz.</h5>
+                <Space></Space>
+                <Link to="/question/1"  className="btn btn-info">
+                    Start
+                </Link>              
+                </div>
+                <Space></Space>
+                <a
+                className="App-link"
+                href="https://africacode.org"
+                target="_blank"
+                rel="noopener noreferrer"
+                >
+                Lesley Loves ACA
+                </a>
         </header>
       </div>
     );
